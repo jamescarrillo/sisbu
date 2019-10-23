371e87dbb54b534e0a0f97d38098c7edfd957d86
James Carrillo <jamescc821@gmail.com>
2019-10-23 09:32:04 -0500

    upgrde

--- src/main/java/com/ogbu/unprg/sisbu/filter/FilterURL.java
+++ src/main/java/com/ogbu/unprg/sisbu/filter/FilterURL.java
@@ -118,7 +118,8 @@ public class FilterURL implements Filter {
         //version_project += "0.04";
         //version_project += "0.05";
         //version_project += "0.06";
-        version_project += "0.07";
+        //version_project += "0.07";
+        version_project += "0.08";
         /*
             0.01 -> 01/08/19
          */
