9f0b4daf12e5588d3a6189e6914e8d19b15a0419
andres <andres@DESKTOP-1T7KAEK>
2020-05-02 17:59:15 -0500

    evaluacion psicologia

--- src/main/webapp/views/subprojects/app/ogbu/servicios/farmacia/entrada/detalle_entrada.js
+++ src/main/webapp/views/subprojects/app/ogbu/servicios/farmacia/entrada/detalle_entrada.js
@@ -2,25 +2,25 @@ var beanPaginationDetalleEntrada;
 var DetalleEntradaSelected;
 var beanRequestDetalleEntrada = new BeanRequest();
 var ObjectTemporal;
-var contadorObject=0;
+var contadorObject = 0;
 var ProductoSelected;
 document.addEventListener("DOMContentLoaded", function () {
-   
+
     document.querySelector("#btnAgregarProducto").onclick = function () {
-       if (validateFormDetalleEntrada()) {
-        ObjectDetalleEntrada= new DetalleEntrada();
-        ObjectDetalleEntrada.iddetalle_entrada=contadorObject++;
-        
-        ObjectDetalleEntrada.entrada= new Entrada(document.querySelector("#txtFechaEntrada").value);
-        ObjectDetalleEntrada.entrada.personal =new Personal(PersonalSelected.idpersonal);
-        ObjectDetalleEntrada.presentacion= new Presentacion(document.querySelector("#txtFechaVencimientoEntrada").value,document.querySelector("#txtCantidadProducto").value);
-        ObjectDetalleEntrada.presentacion.producto=new Producto(ProductoSelected.idproducto,ProductoSelected.nombre);
-        ObjectDetalleEntrada.cantidad=document.querySelector("#txtCantidadProducto").value;
-        listDetalleEntrada.push(ObjectDetalleEntrada);
-        toListDetalleEntrada(listDetalleEntrada);
-        limpiarInput();
-        
-       } 
+        if (validateFormDetalleEntrada()) {
+            ObjectDetalleEntrada = new DetalleEntrada();
+            ObjectDetalleEntrada.iddetalle_entrada = contadorObject++;
+
+            ObjectDetalleEntrada.entrada = new Entrada(document.querySelector("#txtFechaEntrada").value);
+            ObjectDetalleEntrada.entrada.personal = new Personal(PersonalSelected.idpersonal);
+            ObjectDetalleEntrada.presentacion = new Presentacion(document.querySelector("#txtFechaVencimientoEntrada").value, document.querySelector("#txtCantidadProducto").value);
+            ObjectDetalleEntrada.presentacion.producto = new Producto(ProductoSelected.idproducto, ProductoSelected.nombre);
+            ObjectDetalleEntrada.cantidad = document.querySelector("#txtCantidadProducto").value;
+            listDetalleEntrada.push(ObjectDetalleEntrada);
+            toListDetalleEntrada(listDetalleEntrada);
+            limpiarInput();
+
+        }
     };
 
 });
@@ -30,7 +30,7 @@ function toListDetalleEntrada(beanPagination) {
     document.querySelector("#tbodyDetalleEntrada").innerHTML = "";
     let row;
     row =
-            `
+        `
            <div class="dt-widget__item border-success bg-primary text-white mb-0 pl-5">
                 <!-- Widget Info -->
                 <div class="dt-widget__info text-truncate " >
@@ -56,29 +56,16 @@ function toListDetalleEntrada(beanPagination) {
             </div>
         `;
     document.querySelector("#tbodyDetalleEntrada").innerHTML += row;
-    if (beanPagination.length > 0) {
-       
-        beanPagination.forEach(DetalleEntrada => {
-            row =
-                    `
+    if (beanPagination.length == 0) {
+        destroyPagination($('#paginationDetalleEntrada'));
+        return;
+    }
+
+    beanPagination.forEach(DetalleEntrada => {
+        row =
+            `
                  <div class="dt-widget__item border-success  pl-5">
-                    <!-- Widget Extra -->
-                    <div class="dt-widget__extra text-right">
-                      
-                        <!-- Hide Content -->
-                        <div class="hide-content pr-2"">
-                            <!-- Action Button Group -->
-                            <div class="action-btn-group">
-                                <button type="button" class="btn btn-default text-danger dt-fab-btn eliminar-DetalleEntrada" idDetalleEntrada='${DetalleEntrada.iddetalle_entrada}' title="Eliminar" data-toggle="tooltip">
-                                    <i class="icon icon-trash-filled"></i>
-                                </button>
-                              
-                            </div>
-                            <!-- /action button group -->
-                        </div>
-                        <!-- /hide content -->
-                    </div>
-                    <!-- /widget extra -->
+                   
                     <!-- Widget Info -->
                     <div class="dt-widget__info text-truncate " >
                         <p class="mb-0 text-truncate ">
@@ -100,25 +87,39 @@ function toListDetalleEntrada(beanPagination) {
                         </p>
                     </div>
                     <!-- /widget info -->
+                    <!-- Widget Extra -->
+                    <div class="dt-widget__extra text-right">
+                      
+                        <!-- Hide Content -->
+                        <div class="hide-content pr-2"">
+                            <!-- Action Button Group -->
+                            <div class="action-btn-group">
+                                <button type="button" class="btn btn-default text-danger dt-fab-btn eliminar-DetalleEntrada" idDetalleEntrada='${DetalleEntrada.iddetalle_entrada}' title="Eliminar" data-toggle="tooltip">
+                                    <i class="icon icon-trash-filled"></i>
+                                </button>
+                              
+                            </div>
+                            <!-- /action button group -->
+                        </div>
+                        <!-- /hide content -->
+                    </div>
+                    <!-- /widget extra -->
                 </div>
             `;
-            document.querySelector("#tbodyDetalleEntrada").innerHTML += row;
-            $('[data-toggle="tooltip"]').tooltip();
-        });
-        addEventsDetalleEntrada();
-       /* buildPagination(
-                beanPagination.count_filter,
-                parseInt(document.querySelector("#sizePageDetalleEntrada").value),
-                document.querySelector("#pageDetalleEntrada"),
-                $('#modalCargandoDetalleEntrada'),
-                $('#paginationDetalleEntrada'));
-     */           
+        document.querySelector("#tbodyDetalleEntrada").innerHTML += row;
+        $('[data-toggle="tooltip"]').tooltip();
+    });
+    addEventsDetalleEntrada();
+    /* buildPagination(
+             beanPagination.count_filter,
+             parseInt(document.querySelector("#sizePageDetalleEntrada").value),
+             document.querySelector("#pageDetalleEntrada"),
+             $('#modalCargandoDetalleEntrada'),
+             $('#paginationDetalleEntrada'));
+  */
 
 
-    } else {
-        destroyPagination($('#paginationDetalleEntrada'));
-        showAlertTopEnd('warning', 'No se encontraron resultados');
-    }
+
 }
 
 function addEventsDetalleEntrada() {
@@ -126,19 +127,19 @@ function addEventsDetalleEntrada() {
     document.querySelectorAll('.eliminar-DetalleEntrada').forEach(btn => {
         //AGREGANDO EVENTO CLICK
         btn.onclick = function () {
-            DetalleEntradaSelected= findByDetalleEntrada(btn.getAttribute('idDetalleEntrada'));
+            DetalleEntradaSelected = findByDetalleEntrada(btn.getAttribute('idDetalleEntrada'));
             ObjectTemporal = listDetalleEntrada.filter(function (obj) {
                 if (findByDetalleEntrada(obj.iddetalle_entrada).iddetalle_entrada != DetalleEntradaSelected.iddetalle_entrada) {
                     return obj;
-                }else {
-                    
+                } else {
+
                     return undefined;
                 }
-              
-                
+
+
             });
-            listDetalleEntrada.length=0;
-            listDetalleEntrada=ObjectTemporal;
+            listDetalleEntrada.length = 0;
+            listDetalleEntrada = ObjectTemporal;
             toListDetalleEntrada(listDetalleEntrada);
         };
     });
@@ -147,7 +148,7 @@ function addEventsDetalleEntrada() {
 function findByDetalleEntrada(idDetalleEntrada) {
     let DetalleEntrada_;
     listDetalleEntrada.forEach(DetalleEntrada => {
-    
+
         if (idDetalleEntrada == DetalleEntrada.iddetalle_entrada) {
             DetalleEntrada_ = DetalleEntrada;
             return;
@@ -161,31 +162,31 @@ function validateFormDetalleEntrada() {
         showAlertTopEnd('warning', 'Por favor ingrese Fecha');
         document.querySelector("#txtFechaEntrada").focus();
         return false;
-    }else  if (PersonalSelected==undefined) {
+    } else if (PersonalSelected == undefined) {
         showAlertTopEnd('warning', 'Por favor ingrese Personal');
         document.querySelector("#txtPersonalEntrada").focus();
         return false;
-    }else  if (document.querySelector("#txtCantidadProducto").value == "") {
+    } else if (document.querySelector("#txtCantidadProducto").value == "") {
         showAlertTopEnd('warning', 'Por favor ingrese la cantidad');
         document.querySelector("#txtCantidadProducto").focus();
         return false;
-    }else  if (ProductoSelected == undefined) {
+    } else if (ProductoSelected == undefined) {
         showAlertTopEnd('warning', 'Por favor ingrese Producto');
         document.querySelector("#txtProductoEntrada").focus();
         return false;
-    }else  if (document.querySelector("#txtFechaVencimientoEntrada").value == "") {
+    } else if (document.querySelector("#txtFechaVencimientoEntrada").value == "") {
         showAlertTopEnd('warning', 'Por favor ingrese Fecha de Vencimiento');
         document.querySelector("#txtFechaVencimientoEntrada").focus();
         return false;
     }
-    
+
     return true;
 }
 
 function limpiarInput() {
     document.querySelector("#txtCantidadProducto").value = "";
-     document.querySelector("#txtFechaVencimientoEntrada").value="";
-     document.querySelector("#txtProductoEntrada").value="";
-     ProductoSelected=undefined;
+    document.querySelector("#txtFechaVencimientoEntrada").value = "";
+    document.querySelector("#txtProductoEntrada").value = "";
+    ProductoSelected = undefined;
 }
 
